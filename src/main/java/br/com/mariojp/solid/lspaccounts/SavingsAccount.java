1a8f0f57560e3a47112c9377119742c631047ed7
Yasmin <yasminrodrigues.barros@ucsal.edu.br>
2025-08-28 17:20:07 -0300

    SavingsAccount sem método withdraw + não implementa Withdrawable

--- src/main/java/br/com/mariojp/solid/lspaccounts/SavingsAccount.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/SavingsAccount.java
@@ -1,8 +1,13 @@
 package br.com.mariojp.solid.lspaccounts;
 
 public class SavingsAccount extends Account {
-    @Override
-    public void withdraw(double amount) {
-        throw new UnsupportedOperationException("Saque não permitido em poupança");
+ 
+     public void deposit(double amount){
+        this.balance += amount;
     }
+
+    public double getBalance(){
+        return balance;
+    }
+
 }
